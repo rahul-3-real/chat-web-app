18fb62790ccfc96ffed5e7f770fbd865b4747f91
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-10-12 19:58:44 +0530

    Frontend - Initialized Tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./static/css/tailwind.config.css";
+import "./static/css/styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
